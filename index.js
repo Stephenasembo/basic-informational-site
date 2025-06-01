63babb9d6a87ce311c0116cf4bd4a20b4b2644ac
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-01 08:22:43 +0300

    Feat: Extract Path From URL Request
    The path will be used to access the files to be displayed

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const fs = require('fs')
 const url = require('url')
 
 const server = http.createServer((req, res) => {
+  let path = url.parse(req.url, true).path
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write('Hello world')
+  res.write(path)
   res.end()
 })
 
