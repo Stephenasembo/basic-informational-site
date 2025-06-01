bcc7781b80a6444928abe080d0c711d0d0d8ff53
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-01 08:39:55 +0300

    Feat: Handle Routing Based On URL Path
    The appropriate page gets rendered and invalid paths are handled
    by the 404 page

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const url = require('url')
 
 const server = http.createServer((req, res) => {
   let path = url.parse(req.url, true).path
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write(path)
-  res.end()
+  let filename;
+  if(path === '/') {
+    filename = './pages/index.html';
+  } else {
+    filename = './pages' + path + '.html';
+  }
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if(err) {
+      fs.readFile('./pages/404.html', 'utf8', (err, data) => {
+        res.write(data);
+        res.end()
+      })
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
 })
 
 server.listen(8080);
