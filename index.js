ce410bc293bb68fbcb3554166072f94a9b9f1f95
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-06-02 18:14:55 +0300

    Refactor: Use Express To Handle Requests

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 require('dotenv').config();
 
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
+const express = require('express')
+const app = express();
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  let path = url.parse(req.url, true).path
-  let filename;
-  if(path === '/') {
-    filename = './pages/index.html';
-  } else {
-    filename = './pages' + path + '.html';
-  }
+function displayErrorPage() {
+  fs.readFile('./pages/404.html', 'utf8', (err, data) => {
+    res.writeHead(404, {'Content-Type': 'text/html'})
+    res.write(data);
+    res.end()
+  })
+}
+
+app.get('/', (req, res) => {
+  console.log(req.path);
+  let filename = './pages/index.html'
   fs.readFile(filename, 'utf8', (err, data) => {
     if(err) {
-      fs.readFile('./pages/404.html', 'utf8', (err, data) => {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        res.write(data);
-        res.end()
-      })
+      displayErrorPage();
       return;
     }
     res.writeHead(200, {'Content-Type': 'text/html'})
@@ -29,4 +28,30 @@ const server = http.createServer((req, res) => {
   })
 })
 
-server.listen(PORT);
+app.get('/contact-me', (req, res) => {
+  let filename = './pages/contact-me.html'
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if(err) {
+      displayErrorPage();
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+})
+
+app.get('/about', (req, res) => {
+  let filename = './pages/about.html'
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if(err) {
+      displayErrorPage();
+      return;
+    }
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+})
+
+app.listen(PORT);
